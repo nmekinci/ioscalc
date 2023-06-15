733c3570b801dc2f9413cac0791c36597e89f4d8
Must <emailme>
2023-06-15 15:46:54 +0200

    app.js created

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ main.addEventListener("click", (e) => {
     let greys = ["0","1","2","3","4","5","6","7","8","9","."]
     let oranges = ["+","-","X","÷","%"]
     
-    
 
     if (greys.includes(e.target.innerText)){
         console.log(e.target.innerText);
